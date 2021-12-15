5c4ea5bcf4ded3a18b43ab245e7915f267f21172
chloevth <c.vauthier@codeur.online>
2021-12-15 17:01:00 +0100

    blurred bg animation chgmt couleurs

---
+++ blurred-gradient-animation/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blurred Gradient Animation</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <a href="https://www.youtube.com/channel/UCQV9r8ZugNYZWD7ihUm2tpg?view_as=subscriber" target="_blank">Auteur : ThreePixDroid ▶ YouTube
+    </a>
+    <canvas></canvas>
+    <script src="main.js"></script>
+</body>
+</html>
